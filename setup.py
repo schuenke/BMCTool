a4e6ff2fc798ae06e677954c35fa55bc61b45dd0
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-01-06 13:59:29 +0100

    update setup files

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/schuenke/BMCTool',
     install_requires=[
-        'numpy',
         'matplotlib',
         'tqdm',
         'PyYAML',
