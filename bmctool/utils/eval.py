6cba0f9ab85ce499efe14ddb5a036d99611d2a0f
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-02-23 14:08:02 +0100

    adjust clims in plot_z()

--- bmctool/utils/eval.py
+++ bmctool/utils/eval.py
@@ -37,19 +37,19 @@ def plot_z(mz: np.array,
         title = 'Z-Spec'
 
     fig, ax1 = plt.subplots()
-    ax1.set_ylim([0, 1.05])
-    ax1.set_ylabel('Z', color='b')
-    ax1.set_xlabel('Offsets')
+    ax1.set_ylim([round(min(mz) - 0.05, 1), round(max(mz) + 0.05, 1)])
+    ax1.set_ylabel('normalized signal', color='b')
+    ax1.set_xlabel('offset')
     plt.plot(offsets, mz, '.--', label='$Z$', color='b')
     if invert_ax:
         plt.gca().invert_xaxis()
     ax1.tick_params(axis='y', labelcolor='b')
 
     if plot_mtr_asym:
-        mtr_asym = calc_mtr_asym(mz)
+        mtr_asym = calc_mtr_asym(z=mz, offsets=offsets)
 
         ax2 = ax1.twinx()
-        ax2.set_ylim([0, round(np.max(mtr_asym) + 0.01, 2)])
+        ax2.set_ylim([round(min(mtr_asym) - 0.01, 2), round(max(mtr_asym) + 0.01, 2)])
         ax2.set_ylabel('$MTR_{asym}$', color='y')
         ax2.plot(offsets, mtr_asym, label='$MTR_{asym}$', color='y')
         ax2.tick_params(axis='y', labelcolor='y')
