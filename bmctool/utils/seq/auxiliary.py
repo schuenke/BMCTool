ea65df62e3f22dc729d72edde8bbfed64f4aca3d
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-03-31 09:53:52 +0200

    update get_num_adc_events function

--- bmctool/utils/seq/auxiliary.py
+++ bmctool/utils/seq/auxiliary.py
@@ -2,7 +2,9 @@
 auxiliary.py
     Auxiliary functions for seq files.
 """
+import numpy as np
 from pypulseq.Sequence.sequence import Sequence
+from pypulseq.Sequence.read_seq import __strip_line as strip_line
 from bmctool.utils.seq.read import read_any_version
 
 
@@ -23,17 +25,25 @@ def get_offsets(seq: Sequence = None,
     return offsets
 
 
-def get_num_adc_events(seq: Sequence = None,
-                       seq_file: str = None) \
-        -> int:
+def get_num_adc_events(seq_file: str) -> int:
     """
-    Reads number of ADC events (should equal number of offsets).
-    :param seq: Sequence object to get the offsets from
-    :param seq_file: sequence file to read the offsets from
+    Reads number of ADC events in a sequence file
+    :param seq_file: sequence file to read the num_adc_events from
     :return: num_adc_events
     """
-    if not seq and not seq_file:
-        raise ValueError('You need to pass either the sequence filename or the Sequence object.')
-    offsets = get_offsets(seq_file=seq_file)
-    num_adc_events = len(offsets)
-    return num_adc_events
+    adc_event_count = 0
+    with open(seq_file, 'r') as seq:
+        while True:
+            line = strip_line(seq)
+            if line == -1:
+                break
+            elif line == '[BLOCKS]':
+                line = strip_line(seq)
+                while line != '' and line != ' ' and line != '#':
+                    block_event = np.fromstring(line, dtype=int, sep=' ')
+                    if block_event[6] == 1:
+                        adc_event_count += 1  # count number of events before 1st adc
+                    line = strip_line(seq)
+            else:
+                pass
+    return adc_event_count
