8f72fe3e503d8310aded5f5c6c719a78caf0994f
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-06-01 15:54:57 +0200

    adjust simulate and plot_z functions

--- bmctool/utils/eval.py
+++ bmctool/utils/eval.py
@@ -26,46 +26,44 @@ def calc_mtr_asym(z: np.ndarray,
     return np.interp(offsets, x_interp, asym)
 
 
-def plot_z(mz: np.array,
-           offsets: np.array = None,
+def plot_z(y: np.array,
+           x: np.array = None,
            invert_ax: bool = True,
            plot_mtr_asym: bool = False,
-           title: str = None,
-           x_label: str = 'offset',
-           y_label:str = 'normalized signal') \
+           title: str = 'spectrum',
+           x_label: str = 'offsets [ppm]',
+           y_label: str = 'normalized signal',
+           **kwargs) \
         -> Figure:
     """
     initiating calculations and plotting functions
-    :param mz: magnetization vector
-    :param offsets: offsets to plot the magnetization on
+    :param y: y-values
+    :param x: x-values (e.g. offsets, inversion times)
     :param invert_ax: boolean to invert x-axis
     :param plot_mtr_asym: boolean to enable/disable plotting of MTRasym
     :param title: optional title for the plot
     :param x_label: label of x-axis
     :param y_label: label of y-axis
     """
-    if offsets is None:
-        offsets = range(len(mz))
-
-    if title is None:
-        title = 'Z-Spec'
+    if x is None:
+        x = range(len(y))
 
     fig, ax1 = plt.subplots()
-    ax1.set_ylim([round(min(mz) - 0.05, 2), round(max(mz) + 0.05, 2)])
+    ax1.set_ylim([round(min(y) - 0.05, 2), round(max(y) + 0.05, 2)])
     ax1.set_ylabel(y_label, color='b')
     ax1.set_xlabel(x_label)
-    plt.plot(offsets, mz, '.--', label='$Z$', color='b')
+    plt.plot(x, y, '.--', label='$Z$', color='b')
     if invert_ax:
         plt.gca().invert_xaxis()
     ax1.tick_params(axis='y', labelcolor='b')
 
     if plot_mtr_asym:
-        mtr_asym = calc_mtr_asym(z=mz, offsets=offsets)
+        mtr_asym = calc_mtr_asym(z=y, offsets=x)
 
         ax2 = ax1.twinx()
         ax2.set_ylim([round(min(mtr_asym) - 0.01, 2), round(max(mtr_asym) + 0.01, 2)])
         ax2.set_ylabel('$MTR_{asym}$', color='y')
-        ax2.plot(offsets, mtr_asym, label='$MTR_{asym}$', color='y')
+        ax2.plot(x, mtr_asym, label='$MTR_{asym}$', color='y')
         ax2.tick_params(axis='y', labelcolor='y')
         fig.tight_layout()
 
