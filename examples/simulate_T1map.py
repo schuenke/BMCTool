26f05d0b629d22c7894e9c39893a02191f7d0e83
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-06-01 15:55:27 +0200

    adjust examples to new simulate function

--- examples/simulate_T1map.py
+++ examples/simulate_T1map.py
@@ -3,32 +3,23 @@ simulate_T1map.py
     Script to run the BMCTool simulation for a T1 mapping sequence.
 """
 from pathlib import Path
-from bmctool.bmc_tool import BMCTool
-from bmctool.utils.eval import plot_z
-from bmctool.set_params import load_params
+from bmctool.simulate import simulate
 from bmctool.utils.seq.auxiliary import get_definition
 
 # set necessary file paths:
 config_file = Path(__file__).parent / 'library' / 'example_config.yaml'
 seq_file = Path(__file__).parent / 'library' / 'T1map.seq'
 
-# load config file(s) and print settings
-sim_params = load_params(config_file)
-sim_params.print_settings()
-
-# create BMCTool object and run simulation
-sim = BMCTool(sim_params, seq_file)
-sim.run()
-
 # read recovery times from seq file definitions
 TI = get_definition(seq_file, 'TI')
 
-# extract and plot z-spectrum
-offsets, mz = sim.get_zspec()
-fig = plot_z(mz=mz,
-             offsets=TI,
-             invert_ax=False,
-             plot_mtr_asym=False,
-             title='Example T1 saturation recovery curve',
-             x_label='TI [s]')
+sim = simulate(config_file=config_file,
+               seq_file=seq_file,
+               show_plot=True,
+               verbose=True,
+               x=TI,
+               x_label='TI [s]',
+               invert_ax=False,
+               title='Example T1 saturation recovery curve')
+
 
--- examples/simulate_WASABI.py
+++ examples/simulate_WASABI.py
@@ -3,30 +3,16 @@ simulate_WASABI.py
     Script to run the BMCTool simulation for WASABI sequence.
 """
 from pathlib import Path
-from bmctool.bmc_tool import BMCTool
-from bmctool.utils.eval import plot_z
-from bmctool.set_params import load_params
+from bmctool.simulate import simulate
 
 # set necessary file paths:
 config_file = Path(__file__).parent / 'library' / 'example_config.yaml'
 seq_file = Path(__file__).parent / 'library' / 'WASABI.seq'
 
-# load config file(s) and print settings
-sim_params = load_params(config_file)
-sim_params.print_settings()
 
-# create BMCTool object and run simulation
-sim = BMCTool(sim_params, seq_file)
-sim.run()
+sim = simulate(config_file=config_file,
+               seq_file=seq_file,
+               show_plot=True,
+               verbose=False,
+               title='Example WASABI spectrum')
 
-# create BMCToll object and run simulation
-Sim = BMCTool(sim_params, seq_file)
-Sim.run()
-
-# extract and plot z-spectrum
-offsets, mz = sim.get_zspec()
-fig = plot_z(mz=mz,
-             offsets=offsets,
-             invert_ax=True,
-             plot_mtr_asym=False,
-             title='Example WASABI spectrum')
