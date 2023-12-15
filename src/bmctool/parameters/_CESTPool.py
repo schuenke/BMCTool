53befc244a53d9048957bb73a742eea8d844ec17
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-12-15 08:11:11 +0100

    update CESTPool and MTPool to inherit from Pool

--- src/bmctool/parameters/_CESTPool.py
+++ src/bmctool/parameters/_CESTPool.py
@@ -1,30 +1,52 @@
-"""Definition of CESTPool dataclass."""
+"""Definition of CESTPool class."""
 
 from __future__ import annotations
 
-import dataclasses
+from bmctool.parameters._Pool import Pool
 
 
-@dataclasses.dataclass(slots=True)
-class CESTPool:
-    """Class to store CEST pool parameters.
+class CESTPool(Pool):
+    """Class to store CESTPool parameters."""
 
-    Parameters:
-    -----------
-    r1 : float
-        R1 relaxation rate [Hz] (1/T1)
-    r2 : float
-        R2 relaxation rate [Hz] (1/T2)
-    k : float
-        exchange rate [Hz]
-    f : float
-        pool size fraction
-    dw : float
-        chemical shift from water [ppm]
-    """
+    __slots__ = ['_r1', '_r2', '_k', '_f', '_dw']
 
-    r1: float
-    r2: float
-    k: float
-    f: float
-    dw: float
+    def __init__(
+        self,
+        k: float,
+        f: float,
+        dw: float,
+        r1: float = None,
+        r2: float = None,
+        t1: float = None,
+        t2: float = None,
+    ):
+        """Initialize CESTPool object.
+
+        Parameters
+        ----------
+        r1
+            R1 relaxation rate [Hz] (1/T1)
+        r2
+            R2 relaxation rate [Hz] (1/T2)
+        k
+            exchange rate [Hz]
+        f
+            pool size fraction
+        dw
+            chemical shift from water [ppm]
+        """
+
+        super().__init__(f=f, dw=dw, r1=r1, r2=r2, t1=t1, t2=t2)
+        self.k = k
+
+    @property
+    def k(self) -> float:
+        """Exchange rate [Hz]."""
+        return self._k
+
+    @k.setter
+    def k(self, value: float) -> None:
+        value = float(value)
+        if value < 0:
+            raise ValueError('k must be positive.')
+        self._k = value
--- src/bmctool/parameters/_MTPool.py
+++ src/bmctool/parameters/_MTPool.py
@@ -1,33 +1,69 @@
-"""Definition of MTPool dataclass."""
+"""Definition of MTPool class."""
 
 from __future__ import annotations
 
-import dataclasses
+from bmctool.parameters._Pool import Pool
 
 
-@dataclasses.dataclass(slots=True)
-class MTPool:
-    """Class to store MT pool parameters.
+class MTPool(Pool):
+    """Class to store MTPool parameters."""
 
-    Parameters:
-    -----------
-    r1 : float
-        R1 relaxation rate [Hz] (1/T1)
-    r2 : float
-        R2 relaxation rate [Hz] (1/T2)
-    k : float
-        exchange rate [Hz]
-    f : float
-        pool size fraction
-    dw : float
-        chemical shift from water [ppm]
-    lineshape : str
-        Lineshape of the MT pool ("Lorentzian", "SuperLorentzian" or "None")
-    """
+    valid_lineshapes = ['lorentzian', 'superlorentzian']
+    __slots__ = ['_r1', '_r2', '_k', '_f', '_dw', '_lineshape']
 
-    r1: float
-    r2: float
-    k: float
-    f: float
-    dw: float
-    lineshape: str
+    def __init__(
+        self,
+        k: float,
+        f: float,
+        dw: float,
+        lineshape: str,
+        r1: float = None,
+        r2: float = None,
+        t1: float = None,
+        t2: float = None,
+    ):
+        """Initialize MTPool object.
+
+        Parameters
+        ----------
+        r1
+            R1 relaxation rate [Hz] (1/T1)
+        r2
+            R2 relaxation rate [Hz] (1/T2)
+        k
+            exchange rate [Hz]
+        f
+            pool size fraction
+        dw
+            chemical shift from water [ppm]
+        lineshape
+            lineshape of the MT pool ("Lorentzian", "SuperLorentzian")
+        """
+
+        super().__init__(f=f, dw=dw, r1=r1, r2=r2, t1=t1, t2=t2)
+
+        self.k = k
+        self.lineshape = lineshape
+
+    @property
+    def k(self) -> float:
+        """Exchange rate [Hz]."""
+        return self._k
+
+    @k.setter
+    def k(self, value: float) -> None:
+        value = float(value)
+        if value < 0:
+            raise ValueError('k must be positive.')
+        self._k = value
+
+    @property
+    def lineshape(self) -> str:
+        """Lineshape of the MT pool."""
+        return self._lineshape
+
+    @lineshape.setter
+    def lineshape(self, value: str) -> None:
+        if value.lower() not in self.valid_lineshapes:
+            raise ValueError('lineshape must be one of {self.valid_lineshapes}.')
+        self._lineshape = value.lower()
