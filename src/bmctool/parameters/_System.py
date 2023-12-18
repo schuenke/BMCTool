31538a59533492933855e16f3eaf34b1f20e5675
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-12-18 10:27:30 +0100

    update System class refactor to match style of other param classes add tests

--- src/bmctool/parameters/_System.py
+++ src/bmctool/parameters/_System.py
@@ -2,26 +2,86 @@
 
 from __future__ import annotations
 
-import dataclasses
+import operator
 
 
-@dataclasses.dataclass(slots=True)
 class System:
-    """Class to store system parameters.
+    """Class to store system parameters."""
 
-    Parameters:
-    -----------
-    b0 : float
-        field strength [T]
-    gamma : float
-        gyromagnetic ratio [MHz/T]
-    b0_inhom : float
-        _description_
-    rel_b1 : float
-        _description_
-    """
+    __slots__ = ['_b0', '_gamma', '_b0_inhom', '_rel_b1']
 
-    b0: float
-    gamma: float
-    b0_inhom: float
-    rel_b1: float
+    def __init__(
+        self,
+        b0: float,
+        gamma: float,
+        b0_inhom: float,
+        rel_b1: float,
+    ):
+        """Initialize System instance.
+
+        Parameters
+        ----------
+        b0
+            field strength [T]
+        gamma
+            gyromagnetic ratio [MHz/T]
+        b0_inhom
+            B0 field inhomogeneity [ppm]
+        rel_b1
+            B1 field scaling factor
+        """
+
+        # set attributes using setters to check validity
+        self.b0 = b0
+        self.gamma = gamma
+        self.b0_inhom = b0_inhom
+        self.rel_b1 = rel_b1
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if self.__slots__ == other.__slots__:
+                attr_getters = [operator.attrgetter(attr) for attr in self.__slots__]
+                return all(getter(self) == getter(other) for getter in attr_getters)
+        return False
+
+    @property
+    def b0(self) -> float:
+        """Return field strength."""
+        return self._b0
+
+    @b0.setter
+    def b0(self, value: float) -> None:
+        value = float(value)
+        if value < 0:
+            raise ValueError('Field strength must be positive.')
+        self._b0 = value
+
+    @property
+    def gamma(self) -> float:
+        """Return gyromagnetic ratio."""
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, value: float) -> None:
+        self._gamma = float(value)
+
+    @property
+    def b0_inhom(self) -> float:
+        """Return B0 field inhomogeneity."""
+        return self._b0_inhom
+
+    @b0_inhom.setter
+    def b0_inhom(self, value: float) -> None:
+        self._b0_inhom = float(value)
+
+    @property
+    def rel_b1(self) -> float:
+        """Return B1 field scaling factor."""
+        return self._rel_b1
+
+    @rel_b1.setter
+    def rel_b1(self, value: float) -> None:
+        value = float(value)
+        if value < 0:
+            raise ValueError('B1 field scaling factor must be positive.')
+        self._rel_b1 = value
--- src/bmctool/parameters/__init__.py
+++ src/bmctool/parameters/__init__.py
@@ -4,3 +4,4 @@ from bmctool.parameters._MTPool import MTPool
 from bmctool.parameters._WaterPool import WaterPool
 from bmctool.parameters._Options import Options
 from bmctool.parameters._System import System
+from bmctool.parameters._Parameters import Parameters
