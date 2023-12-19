619dfa2e7dcc1daad1bdd663b6bdd30c72b1569e
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-12-19 16:56:33 +0100

    add tests for Parameters class (WIP) add valid_config_dict fixture add valid_yaml_config_file fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,35 @@
+import tempfile
+
 import pytest
+import yaml
+
+
+@pytest.fixture
+def valid_config_dict():
+    return {
+        'water_pool': {
+            'r1': 1.0,
+            'r2': 2.0,
+            'f': 1.0,
+        },
+        'cest_pool': {
+            'cest_1': {'f': 0.001, 't1': 1.0, 't2': 0.1, 'k': 100, 'dw': 1.0},
+            'cest_2': {'f': 0.002, 't1': 2.0, 't2': 0.2, 'k': 2000, 'dw': -2.0},
+        },
+        'verbose': True,
+        'reset_init_mag': True,
+        'scale': 1.0,
+        'max_pulse_samples': 200,
+        'b0': 3.0,
+        'gamma': 42.5764,
+        'b0_inhom': 0.0,
+        'rel_b1': 1.0,
+    }
+
+
+@pytest.fixture()
+def valid_yaml_config_file(valid_config_dict):
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        yaml.dump(valid_config_dict, f)
+    yield f.name
+    f.close()
---
+++ tests/parameters/test_Parameters.py
@@ -0,0 +1,98 @@
+import pytest
+
+from bmctool.parameters import Parameters
+from tests.conftest import valid_config_dict
+from tests.conftest import valid_yaml_config_file
+
+
+def test_init_from_valid_dict(valid_config_dict):
+    """Test that the Parameters class can be instantiated from a valid
+    dictionary."""
+    p = Parameters.from_dict(valid_config_dict)
+
+    # assert that the attributes are set correctly
+    assert p.system.b0 == valid_config_dict['b0']
+    assert p.system.gamma == valid_config_dict['gamma']
+    assert p.system.b0_inhom == valid_config_dict['b0_inhom']
+    assert p.system.rel_b1 == valid_config_dict['rel_b1']
+
+    assert p.options.verbose == valid_config_dict['verbose']
+    assert p.options.reset_init_mag == valid_config_dict['reset_init_mag']
+    assert p.options.scale == valid_config_dict['scale']
+    assert p.options.max_pulse_samples == valid_config_dict['max_pulse_samples']
+
+    assert p.water_pool.r1 == valid_config_dict['water_pool']['r1']
+    assert p.water_pool.r2 == valid_config_dict['water_pool']['r2']
+    assert p.water_pool.f == valid_config_dict['water_pool']['f']
+
+    assert p.cest_pools[0].f == valid_config_dict['cest_pool']['cest_1']['f']
+    assert p.cest_pools[0].t1 == valid_config_dict['cest_pool']['cest_1']['t1']
+    assert p.cest_pools[0].t2 == valid_config_dict['cest_pool']['cest_1']['t2']
+    assert p.cest_pools[0].k == valid_config_dict['cest_pool']['cest_1']['k']
+    assert p.cest_pools[0].dw == valid_config_dict['cest_pool']['cest_1']['dw']
+
+    assert p.cest_pools[1].f == valid_config_dict['cest_pool']['cest_2']['f']
+    assert p.cest_pools[1].t1 == valid_config_dict['cest_pool']['cest_2']['t1']
+    assert p.cest_pools[1].t2 == valid_config_dict['cest_pool']['cest_2']['t2']
+    assert p.cest_pools[1].k == valid_config_dict['cest_pool']['cest_2']['k']
+    assert p.cest_pools[1].dw == valid_config_dict['cest_pool']['cest_2']['dw']
+
+
+def test_init_from_valid_file(valid_config_dict, valid_yaml_config_file):
+    """Test that the Parameters class can be instantiated from a valid YAML
+    config file."""
+    p = Parameters.from_yaml(valid_yaml_config_file)
+    q = Parameters.from_dict(valid_config_dict)
+
+    assert p == q
+
+
+def test_init_from_alternative_param_names(valid_config_dict):
+    """Test that the Parameters class can be instantiated with alternative
+    parameter names."""
+
+    # create copy of valid_config_dict and rename some parameters
+    valid_config_dict_alt_names = valid_config_dict.copy()
+    valid_config_dict_alt_names['relb1'] = valid_config_dict_alt_names.pop('rel_b1')
+    valid_config_dict_alt_names['b0_inhomogeneity'] = valid_config_dict_alt_names.pop('b0_inhom')
+
+    p = Parameters.from_dict(valid_config_dict_alt_names)
+    q = Parameters.from_dict(valid_config_dict)
+
+    assert p == q
+
+
+@pytest.mark.parametrize(
+    'parameter, value',
+    [
+        ('t1', 3.0),
+        ('t1', '5'),
+        ('r1', 0.5),
+        ('f', 0.5),
+    ],
+)
+def test_update_water_pool_parameter(valid_config_dict, parameter, value):
+    """Test that the water pool parameters are updated correctly."""
+    p = Parameters.from_dict(valid_config_dict)
+
+    # call p.update_water_pool() with the parameter and value
+    getattr(p, 'update_water_pool')(**{parameter: value})
+
+    # assert that the attribute was updated correctly
+    assert getattr(p.water_pool, parameter) == float(value)
+
+
+# @pytest.mark.parametrize(
+#     'parameter, value',
+#     [
+#         ('b0', 3.0),
+#         ('gamma', 42.5764),
+#         ('b0_inhom', 0.0),
+#         ('rel_b1', 1.0),
+#     ],
+# )
+# def test_update_waterpool_parameter(parameter, value):
+#     """Test that the water pool parameters are updated correctly."""
+#     p = Parameters()
+#     p.update_waterpool(**{parameter: value})
+#     assert getattr(p.waterpool, parameter) == value
