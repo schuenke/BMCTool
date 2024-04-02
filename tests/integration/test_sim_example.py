e31663e5d2c454dfe018524291836f898422e816
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-04-02 17:04:08 +0200

    fix example_results path

--- tests/integration/test_sim_example.py
+++ tests/integration/test_sim_example.py
@@ -7,7 +7,7 @@ from bmctool.simulation._simulate import sim_example
 def test_sim_example():
     """Test that the example simulation result and reference results match."""
     # load reference results from txt file
-    ref = np.loadtxt(Path(R'tests\integration\example_results.txt'))
+    ref = np.loadtxt(Path(__file__).parent / 'example_results.txt')
 
     # run example simulation
     _, result = sim_example(show_plot=False)
