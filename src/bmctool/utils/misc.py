8c95fb332254c4498b5dff0e4aadb0493a63b1eb
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-07-15 07:36:46 +0200

    make truthy_check spelling insensitive and add tests

--- src/bmctool/utils/misc.py
+++ src/bmctool/utils/misc.py
@@ -3,8 +3,10 @@
 
 def truthy_check(value: bool | int | float | str) -> bool:
     """Check if input value is truthy."""
-    if value in {True, 1, 1.0, 'True'}:
+    if isinstance(value, str):
+        value = value.lower()
+    if value in {True, 1, 1.0, 'true'}:
         return True
-    elif value in {False, 0, 'False'}:
+    elif value in {False, 0, 'false'}:
         return False
     raise ValueError('Input {value} cannot be converted to bool.')
