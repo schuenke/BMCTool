27a45e6039b8be479ca5e615acdb1f88fd25101e
Patrick Schuenke <37338697+schuenke@users.noreply.github.com>
2024-07-24 09:24:07 +0200

    update readme file (#35)
    * fix all internal and external links
    * adjust imports in code snippets
    * remove some outdated text
    * format the whole file

--- src/bmctool/simulation/__init__.py
+++ src/bmctool/simulation/__init__.py
@@ -3,3 +3,4 @@ __all__ = ['BlochMcConnellSolver', 'BMCSim', 'simulate']
 from bmctool.simulation.BlochMcConnellSolver import BlochMcConnellSolver
 from bmctool.simulation.BMCSim import BMCSim
 from bmctool.simulation.simulate import simulate
+from bmctool.simulation.simulate import sim_example
